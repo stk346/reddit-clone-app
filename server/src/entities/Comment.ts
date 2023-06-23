7db02130438ea8f731216820a0524d8156a10c2a
stk346 <stk346@gamil.com>
2023-06-23 17:25:23 +0900

    엔티티 생성 완료

---
+++ server/src/entities/Comment.ts
@@ -0,0 +1,52 @@
+import { BeforeInsert, Column, Entity, Index, JoinColumn, ManyToOne, OneToMany } from "typeorm";
+import User from "./User";
+import Vote from "./Vote";
+import Post from "./Post";
+import BaseEntiy from "./Entity";
+import { Exclude, Expose } from "class-transformer";
+import { makeId } from "../utils/helpers";
+
+@Entity("comments")
+export default class Comment extends BaseEntiy {
+    @Index()
+    @Column()
+    identifier: string;
+
+    @Column()
+    body: string;
+
+    @Column()
+    username: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({name: "username", referencedColumnName: "username"})
+    user: User;
+    
+    @Column()
+    postId: number;
+
+    @ManyToOne(() => Post, (post) => post.comments, {nullable: false})
+    post: Post;
+
+    @Exclude()
+    @OneToMany(() => Vote, (vote) => vote.comment)
+    votes: Vote[];
+
+    protected userVote: number;
+
+    setUserVote(user: User) {
+        const index = this.votes?.findIndex((v) => v.username === user.username);
+        this.userVote = index > -1 ? this.votes[index].value : 0;
+    }
+
+    @Expose() get voteScore(): number {
+        const initialValue = 0;
+        return this.votes?.reduce((previousValue, currentObject) => 
+        previousValue + (currentObject.value || 0), initialValue);
+    }
+
+    @BeforeInsert()
+    makeId() {
+        this.identifier = makeId(8);
+    }
+}
