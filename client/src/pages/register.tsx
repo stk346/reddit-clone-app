4c88435298c3bfa24de02c687e803cb053eaf129
stk346 <stk346@gamil.com>
2023-06-23 19:00:51 +0900

    회원가입 페이지 기능 구현중

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,13 +1,36 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import InputGroup from "../components/InputGroup";
 
-const register = () => {
+const Register = () => {
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState<any>({});
     return (
         <div className="bg-white">
             <div className="flex flex-col items-center justify-content h-screen p-6">
                 <div className='w-10/12 mx-auto md:w-96'>
                     <h1 className="mb-2 text-lg font-medium">회원가입</h1>
                     <form>
+                        <InputGroup
+                            placeholder="Email"
+                            value={email}
+                            setValue={setEmail}
+                            error={errors.email}
+                        />
+                        <InputGroup
+                            placeholder="Username"
+                            value={username}
+                            setValue={setUsername}
+                            error={errors.username}
+                        />
+                        <InputGroup
+                            placeholder="Password"
+                            value={password}
+                            setValue={setPassword}
+                            error={errors.password}
+                        />
                         <button className="w-full py-2 mb-1 text-xs fond-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
                             회원가입
                         </button>
@@ -22,4 +45,4 @@ const register = () => {
     )
 }
 
-export default register;
+export default Register;
