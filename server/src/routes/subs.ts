d88ece06ce418c2af7c8afa8f194fd6e4a5eb975
stk346 <stk346@gamil.com>
2023-06-26 16:48:17 +0900

    user, auth 미들웨어 생성하기

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -1,22 +1,13 @@
-import { Request, Response, Router } from "express";
-import User from "../entities/User";
-import jwt from "jsonwebtoken"
+import { NextFunction, Request, Response, Router } from "express";
+import userMiddleware from "../middlewares/user";
+import authMiddleware from "../middlewares/auth";
 
-const createSub = async (req: Request, res: Response, next) => {
+const createSub = async (req: Request, res: Response, next: NextFunction) => {
     const {name, title, description} = req.body;
-
-    const token = req.cookies.token;
-    if (!token) return next()
-
-    const {username}: any = jwt.verify(token, process.env.JWT_SECRET);
-
-    const user = await User.findOneBy({"username": username});
-
-    if (!user) throw new Error("UnAuthenticated");
 }
 
 const router = Router();
 
-router.post("/", createSub);
+router.post("/", userMiddleware, authMiddleware, createSub);
 
 export default router;
