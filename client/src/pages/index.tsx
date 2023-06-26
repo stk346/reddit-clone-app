8e256b8973e63b91cb87558d8cfd4c42ce362783
stk346 <stk346@gamil.com>
2023-06-26 20:09:42 +0900

    커뮤니티 리스트 생성하기

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,13 +2,30 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <h1 className="text-3xl font-bold underline bg-white">
-        Hello world!
-      </h1>
+    <div className='flex max-w-5xl px-4 pt-5 mx-auto'>
+      {/* 포스트 리스트 */}
+      {/* 포스트 리스트의 사이즈에 따라서 사이즈가 반응형으로 달라지도록 기능 구현*/}
+      <div className='w-full md:mr-3 md:w-8/12'></div>
+
+      {/* 사이드바 */}
+      <div className='hidden w-4/12 ml-3 md:block'>
+        <div className='bg-white border rounded'>
+          <div className='p-4 border-b'>
+            <p className='text-lg font-semibold text-center'>상위 커뮤니티</p>
+          </div>
+          {/* 커뮤니티 리스트 */}
+          <div></div>
+          <div className='w-full py-6 text-center'>
+            <Link href="/subs/create" className='w-full p-2 text-center text-white bg-gray-400 rounded'>
+              커뮤니티 만들기
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
