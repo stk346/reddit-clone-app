a829e081022209eef8dd66827b724395c60ee153
stk346 <stk346@gamil.com>
2023-06-26 19:18:01 +0900

    네비게이션바 생성

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,10 +2,17 @@ import Axios from 'axios'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../context/auth';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
   Axios.defaults.withCredentials = true;
+
+  // login, register 페이지에서는 NavBar를 보여주지 않음
+  const {pathname} = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
+
   return <AuthProvider>
     <Component {...pageProps} />
     </AuthProvider>
