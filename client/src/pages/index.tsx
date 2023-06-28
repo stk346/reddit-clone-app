cd406cacb3ccc5997895fe13b2eaf37020448557
stk346 <stk346@gamil.com>
2023-06-28 16:07:50 +0900

    커뮤니티 리스트 UI 생성

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,8 +3,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import useSWR from "swr";
+import { Sub } from '../types'
+import axios from 'axios'
+import { useAuthState } from '../context/auth'
 
 const Home: NextPage = () => {
+  const {authenticated} = useAuthState();
+
+  const fetcher = async (url: string) => {
+    return await axios.get(url).then(res => res.data)
+  }
+
+  const address = "http://localhost:4000/api/subs/sub/topSubs"
+  const {data: topSubs} = useSWR<Sub[]>(address, fetcher);
+  console.log('topSubs', topSubs);
+
   return (
     <div className='flex max-w-5xl px-4 pt-5 mx-auto'>
       {/* 포스트 리스트 */}
@@ -18,15 +32,39 @@ const Home: NextPage = () => {
             <p className='text-lg font-semibold text-center'>상위 커뮤니티</p>
           </div>
           {/* 커뮤니티 리스트 */}
-          <div></div>
-          <div className='w-full py-6 text-center'>
-            <Link href="/subs/create" className='w-full p-2 text-center text-white bg-gray-400 rounded'>
-              커뮤니티 만들기
-            </Link>
+            <div>
+              {topSubs?.map((sub) => (
+                <div
+                  key={sub.name}
+                  className='flex items-center px-4 py-2 text-xs border-b'
+                >
+                  <Link href={`/r/${sub.name}`}>
+                    <Image
+                      src={sub.imageUrl}
+                      className='rounded-full cursor-pointer'
+                      alt="Sub"
+                      width={24}
+                      height={24}
+                    />
+                  </Link>
+                  <Link
+                    href={`/r/${sub.name}`} className='ml-2 font-bold hover:cursor-pointer'>
+                      /r/{sub.name}
+                  </Link>
+                  <p className='ml-auto font-md'>{sub.postCount}</p>
+                </div>
+              ))}
+            </div>
+            {authenticated &&
+              <div className='w-full py-6 text-center'>
+                <Link href="/subs/create" className='w-full p-2 text-center text-white bg-gray-400 rounded'>
+                  커뮤니티 만들기
+                </Link>
+              </div>
+            }
           </div>
         </div>
       </div>
-    </div>
   )
 }
 
--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -5,6 +5,7 @@ import { isEmpty } from "class-validator";
 import { AppDataSource } from "../data-source";
 import Sub from "../entities/Sub";
 import User from "../entities/User";
+import Post from "../entities/Post";
 
 const createSub = async (req: Request, res: Response, next: NextFunction) => {
     const {name, title, description} = req.body;
@@ -47,8 +48,28 @@ const createSub = async (req: Request, res: Response, next: NextFunction) => {
     }
 }
 
+const topSubs = async (req: Request, res: Response) => {
+    try {
+        const imageUrlExp = `COALESCE(s."imageUrn", 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y')`;
+        const subs = await AppDataSource
+            .createQueryBuilder()
+            .select(`s.title, s.name, ${imageUrlExp} as "imageUrl", count(p.id) as "postCount"`)
+            .from(Sub, "s")
+            .leftJoin(Post, "p", `s.name = p."subName"`)
+            .groupBy('s.title, s.name, "imageUrl"')
+            .orderBy(`"postCount"`, "DESC")
+            .limit(5)
+            .execute();
+        return res.json(subs);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error: "문제가 발생했습니다."});
+    }
+}
+
 const router = Router();
 
 router.post("/", userMiddleware, authMiddleware, createSub);
+router.get("/sub/topSubs", topSubs)
 
 export default router;
