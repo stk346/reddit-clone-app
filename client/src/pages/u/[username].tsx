ea100dc6e402e595bc298f8931c5c4b365a94dfc
stk346 <stk346@gamil.com>
2023-06-29 17:07:10 +0900

    유저 페이지 생성하기

---
+++ client/src/pages/u/[username].tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+import React from "react";
+import useSWR from "swr";
+
+const UserPage = () => {
+    const router = useRouter();
+    const username = router.query.username;
+
+    const {data, error} = useSWR(username ? `/user/${username}` : null)
+}
+
+export default UserPage;
--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,7 @@ import authRoutes from "./routes/auth";
 import subRoutes from "./routes/subs";
 import postRoutes from "./routes/posts";
 import voteRoutes from "./routes/votes";
+import userRoutes from "./routes/users";
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/subs", subRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/votes", voteRoutes)
+app.use("/api/users", userRoutes)
 
 // 스태틱 파일들이 public 폴더 안에 있음을 알려주는 코드. 추가 안하면 액박뜸
 app.use(express.static("public"));
