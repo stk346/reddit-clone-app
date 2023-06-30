0b12d52dba52a4f695ccc8e19265d1398c8969cb
stk346 <stk346@gamil.com>
2023-06-30 15:12:46 +0900

    DNS 수정

--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     domains: [
       "www.gravatar.com",
       "localhost",
-      "ec2-13-124-204-222.ap-northeast-2.compute.amazonaws.com"
+      "ec2-52-79-227-102.ap-northeast-2.compute.amazonaws.com"
     ] // 리소스를 허용할 domain 주소들
   }
 }
